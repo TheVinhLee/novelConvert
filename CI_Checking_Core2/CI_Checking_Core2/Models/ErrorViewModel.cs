24f3cf4fd942c94fbc52533cf08a6eb730e6d325
Britoz <lythevinh2222@gmail.com>
2019-06-02 19:05:45 +1000

    new ad
    ad

---
+++ CI_Checking_Core2/CI_Checking_Core2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CI_Checking_Core2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
